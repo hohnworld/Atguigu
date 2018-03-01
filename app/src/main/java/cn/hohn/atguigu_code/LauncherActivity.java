511070d086a8bde2da96eb06eddd77fd14034f69
hohnworld <292920487@qq.com>
2018-03-01 17:38:11 +0800

    尚硅谷杨光福android2——启动页面到主页面及细节问题(git)

--- app/src/main/java/cn/hohn/atguigu_code/LauncherActivity.java
+++ app/src/main/java/cn/hohn/atguigu_code/LauncherActivity.java
@@ -1,14 +1,52 @@
 package cn.hohn.atguigu_code;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Handler;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 
 public class LauncherActivity extends Activity {
-
+    Handler handler;
+    public boolean isStartedMainActivity=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //在主线程中执行
+                startMainActivity();
+                //关闭当前LauncherActivity
+                finish();
+            }
+        },2000);
+    }
+
+    //启动MainActivity
+    private void startMainActivity() {
+        if(!isStartedMainActivity){
+            isStartedMainActivity=true;
+            startActivity(new Intent(LauncherActivity.this,MainActivity.class));
+        }
+    }
+
+    //触摸屏幕事件(触摸可能响应多次)
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //触摸屏幕输出多次
+        Log.i("onTouchEvent",Thread.currentThread().getName());
+        startMainActivity();
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    protected void onDestroy() {
+        //应用销毁handler队列消息也清除：不再执行postDelayed()
+        handler.removeCallbacksAndMessages(null);
+        super.onDestroy();
     }
 }
---
+++ app/src/main/java/cn/hohn/atguigu_code/MainActivity.java
@@ -0,0 +1,17 @@
+package cn.hohn.atguigu_code;
+
+        import android.os.Bundle;
+        import android.support.annotation.Nullable;
+        import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by xuequ on 2018-03-01.
+ */
+
+public class MainActivity extends FragmentActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
